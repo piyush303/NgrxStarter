895d69c4170968cb1cf0757e6309639a1144d42b
piyush303 <jainpiyush005@gmail.com>
2025-05-15 19:39:08 +0530

    Rendered table data

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { UsersActions, UsersSelectors } from '../../store/users';
+import { User } from '../../store/users/users.model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users-list',
@@ -8,11 +10,24 @@ import { UsersActions, UsersSelectors } from '../../store/users';
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent implements OnInit{
+  protected isEditing = signal(false)
+
   private store = inject(Store);
 
-  protected users$ = this.store.select(UsersSelectors.selectAllUsers)
+  protected users$ = this.store.select(UsersSelectors.selectAllUsers);
+
+  protected userForm = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl('')
+  })
 
   ngOnInit(): void {
     this.store.dispatch(UsersActions.loadUsers())
+  }
+
+  onEdit(user: User) {
+    console.log('onEdit', user)
   }
 }
