a94f3a75d0df3d309f41afc12baea807d1094c52
piyush303 <jainpiyush005@gmail.com>
2025-05-16 13:41:51 +0530

    Added save feature

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -35,4 +35,8 @@ export class UsersListComponent implements OnInit{
 
     this.store.dispatch(UsersActions.updateUser({update: {id: user.id, changes: {...user}}}))
   }
+
+  onSave() {
+    console.log(this.userForm.value)
+  }
 }
